f593aba625adf9c9aa9114e394931984e07b03f9
kimjusnu <junsu4621@naver.com>
2025-11-04 11:25:44 +0900

    feat: not-found페이지 개발 완료

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -45,7 +45,6 @@ export default function NotFound() {
               요청하신 페이지가 존재하지 않거나 이동되었습니다.
             </p>
           </div>
-
           <div className="flex flex-col sm:flex-row gap-3 justify-center">
             <Link
               href="/"
